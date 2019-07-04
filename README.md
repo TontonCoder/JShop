32ab911d1d88f4e8bc69b50d8f61091fc1a3e7c9
TontonCoder <boubakar.dansoko@outlook.com>
2019-07-04 16:57:24 +0200

    update customer repo adding ReadAllCustomer

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-"# JShop" 
-"# JShop" 
+## JSHOP
+
+[En cours de redaction]
+
+Pré-requis:
+
+Procédure:
+
--- src/db/CustomerRepository.java
+++ src/db/CustomerRepository.java
@@ -2,6 +2,7 @@ package db;
 
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,7 +11,8 @@ import java.util.ArrayList;
 import beans.Customer;
 import tools.Ln;
 
-public class CustomerRepository {
+public class CustomerRepository 
+{
 	
 	private JShopDB db;
 	
@@ -24,46 +26,57 @@ public class CustomerRepository {
 		ArrayList<Customer> customers = new ArrayList<Customer>();
 	    String sql = "SELECT * FROM Customer";
 	        
-	        try 
-	        {
-	        	Connection conn = db.getConnection();
-	            Statement stmt = conn.createStatement();
-	            ResultSet rs = stmt.executeQuery(sql);
-	            
-	            
-	            // loop through the result set
-	            while (rs.next()) 
-	            {
-	                Ln.s(rs.getInt("Id") +  "\t" + 
-	                                   rs.getString("firstName") + "\t" +
-	                                   rs.getString("lastName"));
-	                Customer c = new Customer(rs.getString("firstName"), rs.getString("lastName"));
-	                customers.add(c);
-	            }
-	        } 
-	        catch (SQLException e) 
-	        {
-	            System.out.println(e.getMessage());
-	        }
+        try 
+        {
+        	Connection conn = db.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            
+            while (rs.next()) 
+            {
+                Ln.s(rs.getInt("Id") +" - " + rs.getString("firstName") + " - " + rs.getString("lastName"));
+                Customer c = new Customer(rs.getString("firstName"), rs.getString("lastName"));
+                customers.add(c);
+            }
+        } 
+        catch (SQLException e) 
+        {
+            System.out.println(e.getMessage());
+        }
 		
 		return customers;
 	}
 	
 	public void insertCustomer(Customer c)
 	{
-		
+		String sql = "INSERT INTO Customer(firstName,lastName) VALUES(?, ?)";
+		try
+		{
+			Connection conn = db.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, c.getFirstName());
+            pstmt.setString(2, c.getLastName());
+            pstmt.executeUpdate();
+        } 
+		catch (SQLException e) 
+		{
+            System.out.println(e.getMessage());
+        }
 	}
-	public void getCustomer(int id)
+	public Customer getCustomer(int id)
 	{
+		Customer c = null;
+		//Todo: Implements getting Customer by Id statement
+		return c;
 		
 	}
 	public void updateCustomer(Customer c)
 	{
-		
+		//Todo: Implements update statement
 	}
 	public void deleteCustomer(int id)
 	{
-		
+		//Todo: Implements delete statement
 	}
 	public void closeDB()
 	{
@@ -73,7 +86,10 @@ public class CustomerRepository {
 	public static void main(String[] args)
 	{
 		CustomerRepository cr = new CustomerRepository();
+		Customer c = new Customer("Robberto", "Denirou");
+		cr.insertCustomer(c);
 		ArrayList<Customer> cs = cr.getCustomers();
+		cr.closeDB();
 	}
 
 }
